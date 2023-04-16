7997b5f8ffb74a46d6e7a2f7919f3c49b14555f6
Winifredf <Winifred.Dykes@hotmail.com>
2023-04-16 18:03:10 -0400

    printed all info in file with 1 function in array

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 employee_file = open("employees.txt", "r")
 
-print(employee_file.readline())
-print(employee_file.readline())
-print(employee_file.readline())
-print(employee_file.readline())
-print(employee_file.readline())
+print(employee_file.readlines())
+
 
 employee_file.close()
 
