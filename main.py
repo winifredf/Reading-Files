7ea15a863afd97dda3ccde677d7ff351806d5e81
Winifredf <Winifred.Dykes@hotmail.com>
2023-04-16 17:56:44 -0400

    printed 2 lines of employee_file

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 employee_file = open("employees.txt", "r")
 
 print(employee_file.readline())
-
+print(employee_file.readline())
+print(employee_file.readline())
+print(employee_file.readline())
+print(employee_file.readline())
 employee_file.close()
 
